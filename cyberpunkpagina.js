7948a1e7e00c97a9da90f430f72360997d831253
ArthurRomao02 <arthur.romao@etec.sp.gov.br>
2025-03-23 16:25:51 -0300

    Update cyberpunkpagina.js

--- cyberpunkpagina.js
+++ cyberpunkpagina.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Espera a página carregar antes de adicionar o evento
+   
     document.getElementById("hackButton").addEventListener("click", function() {
         alert("🔥 Você hackeou o sistema! Bem-vindo ao mundo Cyberpunk! 🔥");
     });
